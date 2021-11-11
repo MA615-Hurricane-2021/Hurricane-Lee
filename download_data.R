4dc66530704af210fc55f252cbc7443688d076e2
jyli <jinyuli@bu.edu>
2021-11-10 23:43:31 -0500

    added a select_date parameter, which allows we to withdraw all data of one year by set select_date = F to turn off the selection.

--- download_data.R
+++ download_data.R
@@ -1,14 +1,20 @@
 library(dplyr)
 
 download_data <- function(url = 
-           "https://www.ndbc.noaa.gov/view_text_file.php?filename=amrl1h2011.txt.gz&dir=data/historical/stdmet/"){
+           "https://www.ndbc.noaa.gov/view_text_file.php?filename=amrl1h2011.txt.gz&dir=data/historical/stdmet/",
+           select_date = T){
   ### new url1
   suppressMessages(  ###  This stops the annoying messages on your screen.
     file3 <- read_table(url3, col_names = TRUE)
   )
+  if (select_date) {
+    data_new <-  file3 %>% filter(MM == "09" & (DD =="06" | DD =="05" | DD =="04" | DD =="03" | DD =="02") )   
+    return(data_new)
+  }
+  else {
+    return(file3)
+  }
   
-  data_new <-  file3 %>% filter(MM == "09" & (DD =="06" | DD =="05" | DD =="04" | DD =="03" | DD =="02") )   
-  return(data_new)
 }
 # demo
 # df1 <- download_data()
